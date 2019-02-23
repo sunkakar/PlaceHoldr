838fdbcf608111a68c0cb986cbf7869657b31616
Sundeep Kakar <skakar@iu.edu>
2019-02-23 03:16:37 -0500

    Updates

---
+++ precache-manifest.536fa24ea4ecd3903c1931bede51b199.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/placeholdr/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d1432b9373343a1785ab",
+    "url": "/placeholdr/static/js/runtime~main.d1432b93.js"
+  },
+  {
+    "revision": "2d5c594c0b3f550ae679",
+    "url": "/placeholdr/static/js/main.2d5c594c.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/placeholdr/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "2d5c594c0b3f550ae679",
+    "url": "/placeholdr/static/css/main.ff006cae.chunk.css"
+  },
+  {
+    "revision": "b94cb6893bf5d4c95ede91ab86b5a950",
+    "url": "/placeholdr/index.html"
+  }
+];
